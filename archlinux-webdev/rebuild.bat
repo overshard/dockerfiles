48841e4ea23fbf22fcfffbc360a3a7b67b064b64
Isaac Bythewood <isaac@bythewood.me>
2022-06-07 13:07:52 +0000

    Add rebuild scripts for archlinux-webdev

---
+++ archlinux-webdev/rebuild.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+:: For rebuilding and restarting containers after they have been setup.
+
+docker build -t archlinux-webdev:latest https://raw.githubusercontent.com/overshard/dockerfiles/master/archlinux-webdev/Dockerfile
+
+docker rm --force home-container
+docker run --detach --restart always --name home-container --volume home-code:/home/dev/code --volume home-ssh:/home/dev/.ssh archlinux-webdev:latest
+
+PAUSE
