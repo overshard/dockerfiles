d38bcf54708160c4b70138e1a00ab4b4bc3e2486
Isaac Bythewood <isaac@bythewood.me>
2022-07-02 03:11:42 +0000

    Remove -it option

--- archlinux-webdev/backup.sh
+++ archlinux-webdev/backup.sh
@@ -2,10 +2,10 @@
 # docker run -it --rm alpine ash -c "apk add --no-cache docker curl && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/archlinux-webdev/backup.sh | ash"
 
 
-docker run -it --rm --volumes-from bythewood-container -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
+docker run --rm --volumes-from bythewood-container -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
     tar --exclude .venv --exclude node_modules \
     -zcvf /data/bythewood-container-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
 
-docker run -it --rm --volumes-from vannoppen-container -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
+docker run --rm --volumes-from vannoppen-container -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
     tar --exclude .venv --exclude node_modules \
     -zcvf /data/vannoppen-container-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
