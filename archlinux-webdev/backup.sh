65460a649396731e4cb1797cc44bfcb2e0f4b1b5
Isaac Bythewood <isaac@bythewood.me>
2022-07-02 03:29:36 +0000

    Change excludes and update remote run

--- archlinux-webdev/backup.sh
+++ archlinux-webdev/backup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# docker run -it --rm alpine ash -c "apk add --no-cache docker curl && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/archlinux-webdev/backup.sh | ash"
+# To run this without downloading the script, use:
+#   docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock alpine ash -c "apk add --no-cache docker curl && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/archlinux-webdev/backup.sh | ash"
 
 
 docker run --rm --volumes-from bythewood-container -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
-    tar --exclude .venv --exclude node_modules \
-    -zcvf /data/bythewood-container-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
+  tar --exclude .venv --exclude node_modules --exclude media --exclude db.sqlite3 \
+  -zcvf /data/bythewood-container-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
 
 docker run --rm --volumes-from vannoppen-container -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
-    tar --exclude .venv --exclude node_modules \
-    -zcvf /data/vannoppen-container-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
+  tar --exclude .venv --exclude node_modules --exclude media --exclude db.sqlite3 \
+  -zcvf /data/vannoppen-container-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
--- archlinux-webdev/build.sh
+++ archlinux-webdev/build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-# docker run -it --rm alpine ash -c "apk add --no-cache docker curl && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/archlinux-webdev/build.sh | ash"
+# To run this without downloading the script, use:
+#   docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock alpine ash -c "apk add --no-cache docker curl && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/archlinux-webdev/build.sh | ash"
 
 
 docker build --tag archlinux-webdev:latest https://raw.githubusercontent.com/overshard/dockerfiles/master/archlinux-webdev/Dockerfile
 
 docker rm --force bythewood-container
 docker run --detach --restart unless-stopped --name bythewood-container \
-    --volume bythewood-code:/home/dev/code --volume bythewood-ssh:/home/dev/.ssh --volume /var/run/docker.sock:/var/run/docker.sock \
-    archlinux-webdev:latest
+  --volume bythewood-code:/home/dev/code --volume bythewood-ssh:/home/dev/.ssh --volume /var/run/docker.sock:/var/run/docker.sock \
+  archlinux-webdev:latest
 
 docker rm --force vannoppen-container
 docker run --detach --restart unless-stopped --name vannoppen-container \
-    --volume vannoppen-code:/home/dev/code --volume vannoppen-ssh:/home/dev/.ssh --volume /var/run/docker.sock:/var/run/docker.sock \
-    archlinux-webdev:latest
+  --volume vannoppen-code:/home/dev/code --volume vannoppen-ssh:/home/dev/.ssh --volume /var/run/docker.sock:/var/run/docker.sock \
+  archlinux-webdev:latest
