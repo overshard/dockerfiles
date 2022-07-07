08d76eb48ed512fedc9d621c931ca7c6600e288e
Isaac Bythewood <isaac@bythewood.me>
2022-07-07 04:03:52 +0000

    Add curl to webdev scripts

--- webdev/backup.sh
+++ webdev/backup.sh
@@ -2,7 +2,7 @@
 #
 # To run this without downloading the script, use:
 #   docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock alpine \
-#     sh -c "apk add docker && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/webdev/backup.sh | sh"
+#     sh -c "apk add docker curl && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/webdev/backup.sh | sh"
 
 
 docker run --rm --volumes-from bythewood -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
