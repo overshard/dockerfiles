8a0eda37b2176f727820dc7513d41831771750df
Isaac Bythewood <isaac@bythewood.me>
2022-07-07 04:08:11 +0000

    Change backup dir

--- webdev/backup.sh
+++ webdev/backup.sh
@@ -5,10 +5,10 @@
 #     sh -c "apk add docker curl && curl -o- https://raw.githubusercontent.com/overshard/dockerfiles/master/webdev/backup.sh | sh"
 
 
-docker run --rm --volumes-from bythewood -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
+docker run --rm --volumes-from bythewood -v "/C/Users/Isaac Bythewood/Documents/Backups:/data" alpine \
   tar --exclude .venv --exclude node_modules --exclude media --exclude db.sqlite3 \
   -zcvf /data/bythewood-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
 
-docker run --rm --volumes-from vannoppen -v "/C/Users/Isaac Bythewood/Backups:/data" alpine \
+docker run --rm --volumes-from vannoppen -v "/C/Users/Isaac Bythewood/Documents/Backups:/data" alpine \
   tar --exclude .venv --exclude node_modules --exclude media --exclude db.sqlite3 \
   -zcvf /data/vannoppen-`date +%Y-%m-%d`.tar.gz /home/dev/.ssh /home/dev/code
